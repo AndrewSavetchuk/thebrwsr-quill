ae9605dfd85b610d20f59199f1e7fbe2fe8bd851
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2019-06-24 05:01:57 +0300

    upd

--- formats/ctooltip.js
+++ formats/ctooltip.js
@@ -3,26 +3,26 @@ import Inline from '../blots/inline';
 class Ctooltip extends Inline {
   static create(value) {
     const node = super.create(value);
-    node.setAttribute('data-tooltip', value);
+    node.setAttribute('data-value', value);
     node.setAttribute('class', 'span-tooltip');
-    // node.setAttribute('color', 'rgb(230, 0, 0)');
+    node.setAttribute('color', 'rgb(230, 0, 0)');
     return node;
   }
 
   static formats(domNode) {
-    return domNode.getAttribute('data-tooltip');
+    return domNode.getAttribute('data-value');
   }
 
   format(name, value) {
     if (name !== this.statics.blotName || !value) {
       super.format(name, value);
     } else {
-      this.domNode.setAttribute('data-tooltip', value);
+      this.domNode.setAttribute('data-value', value);
     }
   }
 }
 Ctooltip.blotName = 'ctooltip';
-Ctooltip.className = 'data-tooltip';
+Ctooltip.className = 'span-tooltip';
 Ctooltip.tagName = 'span';
 
 export default Ctooltip;
